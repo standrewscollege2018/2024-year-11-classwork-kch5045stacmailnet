6770d58fbc622ef2c2c291d09d01899ab87e6b80
kch5045stacmailnet <158853351+kch5045stacmailnet@users.noreply.github.com>
2024-02-08 14:58:43 +1300

    Add files via upload

---
+++ 8.2.py
@@ -0,0 +1,5 @@
+'''This code takes the lovely person's number and multiplies it by 2'''
+
+#takes the number as an input then prints it doubled
+TheLovelyPersonsNumber = input("May I have a number, please. ")
+print(int(TheLovelyPersonsNumber) * 2)
