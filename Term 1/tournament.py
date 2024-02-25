eb946ab9eadb98db79ca29aac9f78f6a013ce657
kch5045stacmailnet <158853351+kch5045stacmailnet@users.noreply.github.com>
2024-02-26 11:47:07 +1300

    Update tournament.py

--- Term 1/tournament.py
+++ Term 1/tournament.py
@@ -1,17 +1,19 @@
-import random
-
 # empty list to store names
 opp_names = []
+yourScore = 0
+yourFinalScore = 0
+oppScore = 0
+oppFinalScore = 0
 
 ask1 = True
 ask2 = True
-ask3 = True
 print("Hello, welcome to the tournament.")
 
 
 # while loop to continue to ask for team name that isnt blank
 while ask1 == True:
     tname = input("Please enter the name of the opposing team. ").lower()
+    # error catching - integer and blanks
     try:
         int(tname)
         print("Please try again.")
@@ -20,8 +22,13 @@ while ask1 == True:
         if tname.strip(" ") == "":
             print("Please try again.")
         else:
+
             ask1 = False
-            # while loop to ask for opposition's names value
+
+
+
+        """ we want names of the teams, not their members silly.
+            # while loop to ask for opposition's names value (error catching)
             while ask2 == True:
                 name = input("Please enter the names of the opposing team's members. ")
                 try:
@@ -34,22 +41,43 @@ while ask1 == True:
                     elif name.lower() == "done":
                         ask2 = False
                     else:
+                        # adding names
                         name = name.capitalize()
                         opp_names.append(name)
-                        # getting results
-                        result = input("Please enter the results of the team. "))
-                        try:
-                            int(result)
-                            print("Please enter
+"""
+# getting score for your team
+result1 = int(input("Please enter the results of your team. "))
+while ask2 == True:
+    try:
+        int(result1)
+        yourScore = yourScore + result1
+        if result1 == "stop":
+            ask2 = False
+        else:
+            result2 = input("Please enter the results of the opposing team. ")
+            # getting score of opposing team
+            try:
+                int(result2)
+                oppScore = oppScore + result2
+            except:
+                print("Please try again.")
+    except:
+        print("Please try again.")
 
-
-# prints statements
+# prints statement
 try:
-    if len(opp_names) > 1:
-        print(f"There are {len(opp_names)} people in the raffle.")
+    if yourScore > oppScore:
+        yourFinalScore = yourFinalScore + 3
+        print(
+            f"Your team won. Your team gained {yourFinalScore} points, and the opposing team gained {oppFinalScore} points."
+        )
+    elif yourScore == oppScore:
+        print(f"Your team tied with {tname.capitalize()} at {yourFinalScore} points.")
     else:
-        print(f"There is {len(opp_names)} person in the raffle.")
-    print(f"{opp_names[random.randint(0, len(opp_names))]} has won the tournament.")
+        print(
+            f"Your team lost. Your team gained {yourFinalScore} points, and the opposing team gained {oppFinalScore} points."
+        )
+
 except:
     ask1 = True
     ask2 = True
